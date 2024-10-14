c87500dea4e80fd269923bc63d56d60d94a6edca
aguiita <145377675+aguiita@users.noreply.github.com>
2024-10-14 09:23:04 -0300

    Add files via upload

--- listado_cheques.py
+++ listado_cheques.py
@@ -4,12 +4,11 @@ import time
 from datetime import datetime
 vauxi=''
 
-
-def procesarcheques(archivocsv,dnicliente, tipocheque, estado=None, rangofecha=None):
+def procesarcheques(dnicliente, tipocheque, estado=None, rangofecha=None):
     cheques=[]
 
     #abro el archivo csv
-    with open(archivocsv, mode='r') as archivo:
+    with open('listado_cheques.cvs', mode='r') as archivo:
         lector=csv.DictReader(archivo)
     for linea in lector:
         # Filtrar por DNI
@@ -17,22 +16,40 @@ def procesarcheques(archivocsv,dnicliente, tipocheque, estado=None, rangofecha=N
             continue
         
         # filtro por tipo de cheque
-        if linea['tipo de cheque'] != tipocheque:
+        if linea['TIPODECHEQUE'] != tipocheque:
             continue
         
-        # Filtrar por estado si se proporciona
-        if estado and linea['Estado'] != estado:
+        # Filtro por estado si es que hay
+
+        if estado and linea['ESTADO'] != estado:
             continue
         
         # filtro por rango de fechas
         if rangofecha:
+            # si no hay rango de fecha, el if no se hace.
             fechaorigen = datetime.fromtimestamp(int(linea['FechaOrigen']))
+            # me fijo si el valor de fechaorigen cae dentro del rango de fechas q me da el rangofecha
             if not (rangofecha[0] <= fechaorigen <= rangofecha[1]):
                 continue
-        
-        # cuando todos los filtros, agrega el cheque a resultados
+
+# 'Si se encuentra un número de cheque repetido en la misma cuenta para un DNI 
+# dado, mostrar un mensaje de error indicando el problema'
+        cuenta_origen = linea['NUMEROCUENTAORIGEN']
+        nro_cheque = linea['NUMEROCHEQUE']
+        if any(r['NUMEROCHEQUE'] == nro_cheque and r['NUMEROCUENTAORIGEN'] == cuenta_origen for r in resultados):
+                print(f"Error: El numero de cheque esta repetido ({nro_cheque}) para  {cuenta_origen}.")
+                continue
+       
+
+
+
+ # cuando todos los filtros, agrega el cheque a resultados
         resultados.append(linea)
     return resultados
+
+def exportar_a_un_csv(resultados,dnicliente):
+    timestamp=int(time.time())
+   
 
 
 vauxi = ''
